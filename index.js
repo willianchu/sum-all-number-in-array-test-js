e59695869268b183cf916b0857cd533086a6edb8
willianchu <willianchu@hotmail.com>
2022-01-15 23:11:07 -0800

    implemented recursively sum nested array

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+// eslint-disable-next-line func-style
 function sumItems(array) {
-  // Sum all the numbers in the array
+  if (array.length === 0) {
+    return 0;
+  }
+  if (Array.isArray(array[0])) { // if array[0] is an array
+    // send the head (another array) to sumItems and add the tail (the rest of the array) to the sumItens
+    return sumItems(array[0]) + sumItems(array.slice(1));
+  } else {
+    return array[0] + sumItems(array.slice(1)); // if it's not an array, just add the head to the sumItems and the rest of the array to the sumItems
+  }
 }
 
 module.exports = sumItems;
+
